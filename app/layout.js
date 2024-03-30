6718a642f6ea7a8aa229a66569448a1b7da6cd5a
emonsingha209 <emonsingha209@gmail.com>
2024-03-30 21:17:24 +0600

    final v7

--- app/layout.js
+++ app/layout.js
@@ -40,15 +40,15 @@ export const metadata = {
     "Emon Singha, Frontend Developer, ReactJs Developer, NextJs Developer, Web Developer, Web Development",
   author: "Emon Singha",
   robots: "index, follow",
-  // og: {
-  //   title: "Emon Singha | Front End Developer",
-  //   type: "website",
-  //   url: "https://emonsingha.vercel.app/",
-  //   image: "https://emonsingha.vercel.app/img/ogImage.jpg",
-  //   description:
-  //     "I am Emon Singha. I am a passionate Frontend Developer dedicated to creating immersive UI experiences that resonate with users. With a keen eye for detail and a love for clean code, I bring ideas to life through elegant and functional interfaces.",
-  //   site_name: "Emon Singha Portfolio",
-  // },
+  og: {
+    title: "Emon Singha | Front End Developer",
+    type: "website",
+    url: "https://emonsingha.vercel.app/",
+    image: "https://emonsingha.vercel.app/img/ogImage.jpg",
+    description:
+      "I am Emon Singha. I am a passionate Frontend Developer dedicated to creating immersive UI experiences that resonate with users. With a keen eye for detail and a love for clean code, I bring ideas to life through elegant and functional interfaces.",
+    site_name: "Emon Singha Portfolio",
+  },
 };
 
 export default function RootLayout({ children }) {
